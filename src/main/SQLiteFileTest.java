ffd40fc9c42a04e4759e40ccfa3831e5dafa1815
Michael Massarelli <massarellimj@gmail.com>
2017-09-21 14:30:12 -0500

    Add files via upload

---
+++ src/main/SQLiteFileTest.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.unitedgasket.fulcrum.Fulcrum.db;
+
+import com.unitedgasket.fulcrum.Fulcrum.db.sqlite.MetaDataSource;
+
+/**
+ * 
+ *
+ * @author Michael Massarelli
+ */
+public class SQLiteFileTest {
+    public static final String TAG = "SQLiteFileTest";
+    
+    public static void main(String[] args) {
+	new MetaDataSource();
+    }
+}
